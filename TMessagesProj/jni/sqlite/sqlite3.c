bba31d34dcc58d1d062cf1c5c69dc645211a688f
DrKLO <drklo.2kb@gmail.com>
2015-12-09 21:27:52 +0300

    Update to 3.3.2

--- TMessagesProj/jni/sqlite/sqlite3.c
+++ TMessagesProj/jni/sqlite/sqlite3.c
@@ -17,6 +17,7 @@
 ** language. The code for the "sqlite3" command-line shell is also in a
 ** separate file. This file contains only code for the core SQLite library.
 */
+#define SQLITE_THREADSAFE 0
 #define SQLITE_CORE 1
 #define SQLITE_AMALGAMATION 1
 #ifndef SQLITE_PRIVATE
