9f1a4d4bc10957fbb755ae05147180787ebdb72a
23rd <23rd@vivaldi.net>
2025-09-20 11:05:36 +0300

    Reduced long-click time to context menu appearing from sticker preview.

--- TMessagesProj/src/main/java/org/telegram/ui/ContentPreviewViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ContentPreviewViewer.java
@@ -1824,7 +1824,7 @@ public class ContentPreviewViewer {
                 }
                 if ((newSet != null || contentType == CONTENT_TYPE_EMOJI) && (delegate == null || delegate.needMenu())) {
                     AndroidUtilities.cancelRunOnUIThread(showSheetRunnable);
-                    AndroidUtilities.runOnUIThread(showSheetRunnable, menuOpenDelay > 0 ? menuOpenDelay : 1300);
+                    AndroidUtilities.runOnUIThread(showSheetRunnable, menuOpenDelay > 0 ? menuOpenDelay : 700);
                 }
                 TLRPC.TL_messages_stickerSet stickerSet = MediaDataController.getInstance(currentAccount).getStickerSet(newSet, true);
                 if (stickerSet != null && stickerSet.documents.isEmpty()) {
@@ -1875,7 +1875,7 @@ public class ContentPreviewViewer {
                 }
                 if (delegate.needMenu()) {
                     AndroidUtilities.cancelRunOnUIThread(showSheetRunnable);
-                    AndroidUtilities.runOnUIThread(showSheetRunnable, menuOpenDelay > 0 ? menuOpenDelay : 1300);
+                    AndroidUtilities.runOnUIThread(showSheetRunnable, menuOpenDelay > 0 ? menuOpenDelay : 700);
                 }
             }
         } else {
