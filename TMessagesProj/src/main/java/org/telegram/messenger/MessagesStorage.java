128c08cc8d2fe85fc8bcfa5a267b941de0f0c706
23rd <23rd@vivaldi.net>
2026-04-22 16:19:16 +0300

    Deduplicated chat queries.

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesStorage.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesStorage.java
@@ -3979,6 +3979,8 @@ public class MessagesStorage extends BaseController {
                 }
                 Collections.reverse(messages);
 
+                HashSet<Long> loadedUserIds = new HashSet<>(usersToLoad);
+                HashSet<Long> loadedChatIds = new HashSet<>(chatsToLoad);
                 usersToLoad.clear();
                 chatsToLoad.clear();
                 cursor = database.queryFinalized("SELECT uid, sid, date, expire_date, localName, flags FROM story_pushes");
@@ -3986,12 +3988,13 @@ public class MessagesStorage extends BaseController {
                 while (cursor.next()) {
                     long dialogId = cursor.longValue(0);
                     if (dialogId >= 0) {
-                        if (!usersToLoad.contains(dialogId)) {
+                        if (!loadedUserIds.contains(dialogId) && !usersToLoad.contains(dialogId)) {
                             usersToLoad.add(dialogId);
                         }
                     } else {
-                        if (!chatsToLoad.contains(dialogId)) {
-                            chatsToLoad.add(dialogId);
+                        long chatId = -dialogId;
+                        if (!loadedChatIds.contains(chatId) && !chatsToLoad.contains(chatId)) {
+                            chatsToLoad.add(chatId);
                         }
                     }
                     int id = cursor.intValue(1);
