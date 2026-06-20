4d7f33b218c20055e50633a08ad491074b12975a
23rd <23rd@vivaldi.net>
2026-06-21 00:27:18 +0300

    Added option to disable tablet mode.

--- TMessagesProj/src/main/java/org/telegram/ui/ForkSettingsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ForkSettingsActivity.java
@@ -111,6 +111,7 @@ public class ForkSettingsActivity extends BaseFragment {
 
     public static final int ID_UNIFIED_PUSH = 80;
     public static final int ID_UPDATE_CHECK_INTERVAL = 81;
+    public static final int ID_DISABLE_TABLET_MODE = 82;
     public static final int ID_LOCK_PREMIUM = 83;
 
     public static final int ID_LASTFM_LOGIN = 90;
@@ -520,6 +521,11 @@ public class ForkSettingsActivity extends BaseFragment {
         items.add(UItem.asHeader(LocaleController.getString(R.string.ForkSectionSystem)));
         items.add(UItem.asSettingsCell(ID_UNIFIED_PUSH, LocaleController.getString(R.string.UnifiedPush), null));
         items.add(UItem.asSettingsCell(ID_UPDATE_CHECK_INTERVAL, LocaleController.getString(R.string.UpdateCheckInterval), getUpdateIntervalText()));
+        if (AndroidUtilities.isTabletInternal()) {
+            items.add(UItem.asButtonCheck(ID_DISABLE_TABLET_MODE, LocaleController.getString(R.string.DisableTabletMode), LocaleController.getString(R.string.DisableTabletModeInfo))
+                .setChecked(SharedConfig.forceDisableTabletMode)
+                .setMultiline(true));
+        }
         items.add(UItem.asButtonCheck(ID_LOCK_PREMIUM, LocaleController.getString(R.string.LockPremium), LocaleController.getString(R.string.LockPremiumInfo))
             .setChecked(pref("lockPremium", false)).setMultiline(true));
         items.add(UItem.asShadow(null));
@@ -657,6 +663,17 @@ public class ForkSettingsActivity extends BaseFragment {
             presentFragment(new NotificationsSettingsActivity().highlightUnifiedPush());
         } else if (id == ID_UPDATE_CHECK_INTERVAL) {
             showUpdateIntervalDialog();
+        } else if (id == ID_DISABLE_TABLET_MODE) {
+            SharedConfig.toggleForceDisableTabletMode();
+            setCellChecked(view, SharedConfig.forceDisableTabletMode);
+            Activity activity = getParentActivity();
+            if (activity != null) {
+                final android.content.pm.PackageManager pm = activity.getPackageManager();
+                final Intent intent = pm.getLaunchIntentForPackage(activity.getPackageName());
+                activity.finishAffinity();
+                activity.startActivity(intent);
+            }
+            System.exit(0);
         } else if (id == ID_LOCK_PREMIUM) {
             toggle("lockPremium", item, view);
 
