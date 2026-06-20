b50ba8d53d4ff971818eb7752b91b27acce97857
23rd <23rd@vivaldi.net>
2026-06-20 23:58:32 +0300

    Fixed AI Editor button ignoring hide setting when sending media.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/CaptionPhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/CaptionPhotoViewer.java
@@ -529,7 +529,7 @@ public class CaptionPhotoViewer extends CaptionContainerView {
 
     private boolean shownAiButton;
     private void showAiButton(boolean show_) {
-        final boolean show = show_;
+        final boolean show = show_ && !MessagesController.getGlobalMainSettings().getBoolean("hideAiEditor", false);
 
         if (shownAiButton == show) return;
         if (show) {
