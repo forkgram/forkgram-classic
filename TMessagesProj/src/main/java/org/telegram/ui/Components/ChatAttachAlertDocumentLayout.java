forkgram/TelegramAndroid

Added new option to disable recent files from attachment alert.

TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlertDocumentLayout.java
@@ -905,6 +905,10 @@ public class ChatAttachAlertDocumentLayout extends ChatAttachAlert.AttachAlertLa
     }
 
     public void loadRecentFiles() {
+        if (MessagesController.getGlobalMainSettings().getBoolean("disableSlideToNextChannel", false)) {
+            listAdapter.recentItems.clear();
+            return;
+        }
         try {
             if (isSoundPicker) {
                 String[] projection = {

forkgram/forkgram-classic

TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlertDocumentLayout.java
@@ -927,6 +927,10 @@ public class ChatAttachAlertDocumentLayout extends ChatAttachAlert.AttachAlertLa
     }
 
     public void loadRecentFiles() {
+        if (MessagesController.getGlobalMainSettings().getBoolean("disableRecentFilesAttachment", false)) {
+            listAdapter.recentItems.clear();
+            return;
+        }
         try {
             if (isSoundPicker) {
                 String[] projection = {